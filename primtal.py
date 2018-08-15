5a3d00c6a987d39d32669f033c1cd89bf8f30c20
Henrik Riskær <hriskaer@gmail.com>
2018-08-15 12:32:15 +0200

    On branch master  Changes to be committed: 	modified:   primtal.py

--- primtal.py
+++ primtal.py
@@ -1,12 +1,5 @@
+for n in range(2,101):
+    for i in range(2,101):
+        if(n % i != 0):
+            print(str(n))
 
-
-def is_prime(n):
-    for i in range(2,n+1):
-        print('...'+str(i))
-
-        if(n % i == 0):
-            return False
-        else:
-            return True
-
-print(is_prime(5))
