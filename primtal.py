e399463befd6d08a55816fb754c6b6e2e2124048
Henrik Riskær <hriskaer@gmail.com>
2018-08-16 12:08:45 +0200

    On branch master  Changes to be committed: 	modified:   primtal.py

--- primtal.py
+++ primtal.py
@@ -1,5 +1,7 @@
-for n in range(2,101):
-    for i in range(2,101):
-        if(n % i != 0):
-            print(str(n))
-
+for n in range(900,1001):
+    if(n > 1):
+        for i in range(2,n):
+            if (n % i) == 0:
+                break
+            else:
+                print(n)
