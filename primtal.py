7e6171bb83c5117dee0d65161f3317355937e19f
Henrik Riskær <hriskaer@gmail.com>
2018-10-04 10:10:25 +0200

    On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   primtal.py

--- primtal.py
+++ primtal.py
@@ -1,7 +1,8 @@
-for n in range(2,10):
-    if(n > 1):
-        for i in range(2,n):
-            if (n % i) == 0:
-                break
-            else
-                print(n)
+for n in range(2,500):
+    isPrime = True
+    for i in range(2,n):
+        if (n % i) == 0:
+            isPrime = False
+            break
+    if (isPrime):
+        print(n)
